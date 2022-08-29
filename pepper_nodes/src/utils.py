bbb9fe441749831c11adccaea48445b17a280655
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-08-29 15:53:43 +0200

    Update utils.py

--- pepper_nodes/src/utils.py
+++ pepper_nodes/src/utils.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 
-from application_node import SingletonMeta
+import qi
+class SingletonMeta(type):
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class Application(metaclass=SingletonMeta):
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self._application = qi.Application(url=f"tcp://{self.ip}:{self.port}")
+
+    def reconnect(self):
+        self._application.stop()
+        self._application = qi.Application(url=f"tcp://{self.ip}:{self.port}")
+        self._application.start()
+        self._application.run()
 
 class Session:
     def __init__(self):
