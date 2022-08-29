a2bcf1297f2bbfcdc343157b472027aa40513b8d
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-08-29 16:02:55 +0200

    Update utils.py

--- pepper_nodes/src/utils.py
+++ pepper_nodes/src/utils.py
@@ -9,6 +9,7 @@ class SingletonMeta(type):
         if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
+            print(cls._instances)
         return cls._instances[cls]
 
 class Application(metaclass=SingletonMeta):
