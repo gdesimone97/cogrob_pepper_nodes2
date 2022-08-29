60f9ed0b59c8859ed86a2a074cbd5dc6a14cc375
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-08-29 15:53:45 +0200

    Update application_node.py

--- pepper_nodes/src/application_node.py
+++ pepper_nodes/src/application_node.py
@@ -1,29 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-import qi
-
-class SingletonMeta(type):
-
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-class Application(metaclass=SingletonMeta):
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-        self._application = qi.Application(url=f"tcp://{self.ip}:{self.port}")
-
-    def reconnect(self):
-        self._application.stop()
-        self._application = qi.Application(url=f"tcp://{self.ip}:{self.port}")
-        self._application.start()
-        self._application.run()
+from utils import Application
 
 if __name__ == "__main__":
     ip = rospy.get_param("ip")
