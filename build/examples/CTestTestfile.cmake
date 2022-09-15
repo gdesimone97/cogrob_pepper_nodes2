09f06bb08722f17576aa4fffbb8cf816ffe1b8c0
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-09-15 19:16:31 +0200

    clean
    clean

--- build/examples/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peppo97/Documents/Github/cogrob_pepper_nodes/src/examples
-# Build directory: /home/peppo97/Documents/Github/cogrob_pepper_nodes/build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- devel/share/pepper_nodes/cmake/pepper_nodes-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(pepper_nodes_MESSAGE_FILES "")
-set(pepper_nodes_SERVICE_FILES "/home/peppo97/Documents/Github/cogrob_pepper_nodes/src/pepper_nodes/srv/ExecuteJS.srv;/home/peppo97/Documents/Github/cogrob_pepper_nodes/src/pepper_nodes/srv/LoadUrl.srv;/home/peppo97/Documents/Github/cogrob_pepper_nodes/src/pepper_nodes/srv/Rest.srv;/home/peppo97/Documents/Github/cogrob_pepper_nodes/src/pepper_nodes/srv/Text2Speech.srv;/home/peppo97/Documents/Github/cogrob_pepper_nodes/src/pepper_nodes/srv/WakeUp.srv")
