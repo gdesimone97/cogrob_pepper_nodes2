e8a91088a5a3975b6827429806addf83a9141c31
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-08-31 01:59:42 +0200

    docker rdp

--- .devcontainer/install_qi.bash
+++ .devcontainer/install_qi.bash
@@ -1,9 +1,17 @@
-if [[ $(uname -m) == "x86_64" ]];
-then
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "x86_64" ]]; then
     echo "Installing qi for x86_64"
     pip install qi
-else
+elif [[ "$ARCH" == "aarch64" ]]; then
     echo "Installing qi for aarch64"
-    wget https://github.com/gdesimone97/cogrob_pepper_nodes/releases/download/qi-3.1.5/qi-3.1.5-cp310-cp310-linux_aarch64.whl
+    QI_WHL_URL="https://github.com/gdesimone97/cogrob_pepper_nodes/releases/download/qi-3.1.5/qi-3.1.5-cp310-cp310-linux_aarch64.whl"
+    wget "$QI_WHL_URL"
     pip install ./qi-3.1.5-cp310-cp310-linux_aarch64.whl
+    rm qi-3.1.5-cp310-cp310-linux_aarch64.whl
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
 fi
