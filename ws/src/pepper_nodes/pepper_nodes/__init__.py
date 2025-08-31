975aa67c01fd1d68486a0ac215e4107298ad0f2f
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-08-31 16:41:16 +0200

    pepper examples pkg

--- ws/src/pepper_nodes/pepper_nodes/__init__.py
+++ ws/src/pepper_nodes/pepper_nodes/__init__.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.parameter import Parameter
+from abc import ABC, abstractmethod
 import re
 
 class PepperIPNotSetError(Exception):
@@ -17,3 +18,66 @@ class PepperNode(Node):
         elif not re.match(r"\d+\.\d+\.\d+\.\d+", self.ip):
             raise Exception("I expected an IP received: {}".format(self.ip))
         self.port = self.declare_parameter("pepper_port", 9559).get_parameter_value().integer_value
+
+class Context:
+    """
+    The Context class is responsible for managing the current state of the application.
+
+    It allows for transitioning between different states and delegates the handling of 
+    operations to the current state.
+
+    Attributes:
+        _state (State): The current state of the context.
+
+    Methods:
+        __init__(state): Initializes the context with a given state.
+        transition_to(state): Transitions the context to a new state.
+        run(): Executes the handle method of the current state.
+    """
+
+    _state = None
+
+    def __init__(self, state) -> None:
+        self.transition_to(state)
+
+    def transition_to(self, state):
+        print(f"Context: Transition to {type(state).__name__}")
+        self._state = state
+        self._state.context = self
+
+    def run(self):
+        self._state.handle()
+
+class State(ABC):
+    """
+    Abstract base class representing a state in a state machine.
+
+    This class defines the interface for a state, including a context
+    property that allows the state to access shared data or functionality
+    from the context in which it operates. Subclasses must implement the
+    `handle` method to define the behavior of the state.
+
+    Attributes:
+        context (Context): The context in which the state operates.
+
+    Methods:
+        handle(): Abstract method that must be implemented by subclasses
+                  to define the state's behavior.
+    """
+    
+    @property
+    def context(self) -> Context:
+        return self._context
+
+    @context.setter
+    def context(self, context: Context) -> None:
+        self._context = context
+
+    @abstractmethod
+    def handle(self) -> None:
+        pass
+
+class StateROS(State):
+    def __init__(self, node: Node):
+        super().__init__()
+        self.node = node
