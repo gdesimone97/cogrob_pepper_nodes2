fe87d8a9b48ec9c96b63b09c4c563c7229901ee4
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-09-01 15:24:10 +0200

    text2speech updates

--- ws/src/examples/examples/tts_example.py
+++ ws/src/examples/examples/tts_example.py
@@ -2,6 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
+from time import sleep
 from pepper_interfaces.srv import Text2Speech
 
 class TTSHandler(Node):
@@ -13,15 +14,17 @@ class TTSHandler(Node):
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waiting for /tts service...')
 
-    def call(self, text: str):
+    def call(self, text: str, language="", volume=Text2Speech.Request.NO_SET):
         request = Text2Speech.Request()
         request.speech = text
+        request.language = language
+        request.volume = volume
 
         future = self.cli.call_async(request)
         rclpy.spin_until_future_complete(self, future)
 
         if future.result() is not None:
-            self.get_logger().info(f"Response: {future.result().result}")
+            self.get_logger().info(f"Response: {future.result().ack}")
         else:
             self.get_logger().error("Service call failed")
 
@@ -30,7 +33,10 @@ def main():
     node = TTSHandler()
 
     text = "Hi! I'm Pepper"
-    node.call(text)
+    node.call(text, language=Text2Speech.Request.ENGLISH, volume=70)
+    sleep(2.0)
+    text = "Ciao! Sono Pepper"
+    node.call(text, language=Text2Speech.Request.ITALIAN)
 
     node.destroy_node()
     rclpy.shutdown()
--- ws/src/pepper_nodes/pepper_nodes/text2speech_node.py
+++ ws/src/pepper_nodes/pepper_nodes/text2speech_node.py
@@ -11,21 +11,46 @@ class Text2SpeechNode(PepperNode):
     def __init__(self):
         super().__init__('text2speech_node')
         self.session = Session(self.ip, self.port)
-        self.tts = self.session.get_service("ALTextToSpeech")
-
+        self.tts_proxy = self.session.get_service("ALTextToSpeech")
+        self.audio_device_proxy = self.session.get_service("ALAudioDevice")
         self.tts_service = self.create_service(Text2Speech, 'tts', self.say_callback)
-
         self.get_logger().info("Text2SpeechNode initialized")
 
-    def say_callback(self, request, response):
+    def set_volume(self, volume: int):
+        if volume == Text2Speech.Request.NO_SET:
+            return
+        if volume not in range(0, 101):
+            self.get_logger().error("Volume not set for value {}. Must be between [0-100].".format(volume))
+            return
+        volume = int(volume)
+        self.audio_device_proxy.setOutputVolume(volume)
+        self.get_logger().info(f"Setting volume to {volume}")
+    
+    def set_language(self, language: str):
+        if language == "":
+            return
+        if language not in [Text2Speech.Request.ENGLISH, Text2Speech.Request.ITALIAN]:
+            self.get_logger().error(f"Language not set for value {language}. Must be one among {[Text2Speech.Request.ENGLISH, Text2Speech.Request.ITALIAN]}")
+            return
+        self.tts_proxy.setLanguage(language)
+        self.get_logger().info(f"Language set to {language}")
+    
+    def say_callback(self, request: Text2Speech.Request, response: Text2Speech.Response):
         try:
-            self.tts.say(request.speech)
+            speech = request.speech
+            self.set_volume(request.volume)
+            self.set_language(request.language)
+            self.tts_proxy.say(speech)
         except Exception as e:
             self.get_logger().warn(f"TTS failed, retrying: {e}")
             self.session.reconnect()
-            self.tts = self.session.get_service("ALTextToSpeech")
-            self.tts.say(request.speech)
-        response.result = "ACK"
+            self.tts_proxy = self.session.get_service("ALTextToSpeech")
+            self.audio_device_proxy = self.session.get_service("ALAudioDevice")
+            speech = request.speech
+            self.set_volume(request.volume)
+            self.set_language(request.language)
+            self.tts_proxy.say(speech)
+        response.ack = "ACK"
         return response
 
 def main():
