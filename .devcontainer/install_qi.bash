597fea950c4d6d7f4ce3e4115ea403177f1cbec8
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-09-02 16:27:33 +0200

    update bash file

--- .devcontainer/install_qi.bash
+++ .devcontainer/install_qi.bash
@@ -5,7 +5,7 @@ ARCH=$(uname -m)
 if [[ "$ARCH" == "x86_64" ]]; then
     echo "Installing qi for x86_64"
     pip install qi
-elif [[ "$ARCH" == "aarch64" ]]; then
+elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
     echo "Installing qi for aarch64"
     QI_WHL_URL="https://github.com/gdesimone97/cogrob_pepper_nodes/releases/download/qi-3.1.5/qi-3.1.5-cp310-cp310-linux_aarch64.whl"
     wget "$QI_WHL_URL"
