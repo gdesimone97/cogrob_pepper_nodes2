adca80e2c165ff45bd1777af4d522efbea995f14
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-09-04 10:50:00 +0200

    service example

---
+++ test_ws/src/service_example/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'service_example'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mivia',
+    maintainer_email='44608428+gdesimone97@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'server = service_example.server:main',
+            'client = service_example.client:main',
+        ],
+    },
+)
